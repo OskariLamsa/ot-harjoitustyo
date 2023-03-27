bd7d1ff57219453801b5267df753148256e1f701
Oskari <oskari.lamsa@helsinki.fi>
2023-03-27 19:59:31 +0300

    Vaatimusmäärittely

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,19 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    def test_lataaminen_oikein(self):
+        self. maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    def test_raha_vahenee(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+    def test_saldo_ei_muutu_jos_on_koyha(self):
+        self.maksukortti.ota_rahaa(5000000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    def test_True_jos_rahat_riittaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(2), True)
+    def test_False_jos_rahat_ei_riita(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(20000), False)
